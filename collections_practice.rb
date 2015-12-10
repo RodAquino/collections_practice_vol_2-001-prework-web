0ea5cc1894c538f0a1227bff520862cd398bd809
RodAquino <rodrigo_59@hotmail.com>
2015-12-10 22:43:05 +0000

    nitrous-backup at Thu Dec 10 22:43:05 UTC 2015

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,83 @@
 # your code goes here
+
+def begins_with_r(tools)
+  tools.each do |word|
+    answer = word[0].include? "r"
+    if answer == false
+      return false
+    end
+    end
+  return true
+    end
+
+def contain_a(element)
+  new_array = []
+  element.each do |word|
+    answer = word.include? 'a'
+    if answer == true
+      new_array << word
+    end
+  end
+  return new_array
+  end
+
+def first_wa(element)
+  element.each do |word|
+    answer = word[0..1].include? "wa"
+    if answer == true
+      return word
+  end
+  end
+end
+
+def remove_non_strings(array)
+  new_array = []
+  array.each do |name|
+    answer = name == name.to_s
+    if answer == false
+      array.delete(name)
+    else
+      new_array << name
+    end
+
+  end
+  return new_array
+end
+
+def count_elements
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
